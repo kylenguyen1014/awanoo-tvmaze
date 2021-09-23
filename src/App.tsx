e1f06282d9dbdf324a203616b9bc4573270ae934
kylenguyen1014 <trba.nguyen@gmail.com>
2021-09-22 23:52:30 -0400

    craft a layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import SearchShow from './Page/SearchShow';
+import People from './Page/People';
+import Shows from './Page/Shows';
+import Box from '@mui/material/Box';
+import { AppBar, Button, Container, Grid, Toolbar, Typography } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box>
+        <AppBar position='static' color='transparent'>
+          <Toolbar>
+            <Grid container justifyContent='space-between'>
+              <Grid item>
+                <Typography variant='h6' component='div' >
+                  AwanooTv
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Grid container spacing={1}>
+                  <Grid item>
+                    <Button variant='text' color='inherit'>Shows</Button>
+                  </Grid>
+                  <Grid item>
+                    <Button variant='text' color='inherit'>Search Show</Button>
+                  </Grid>
+                  <Grid item>
+                    <Button variant='text' color='inherit'>Search People</Button>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Container maxWidth='lg' fixed>
+        <Switch>
+          <Route path='/shows'>
+            <Shows />
+          </Route>
+          <Route path='/search-show'>
+            <SearchShow />
+          </Route>
+          <Route path='/search-peple'>
+            <People />
+          </Route>
+        </Switch>
+      </Container>
     </div>
   );
 }
